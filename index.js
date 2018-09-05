a57aa492f47ce2e5f28b6b7bfd3f168067947de2
Mats Sibelius <mats.sibelius@autosolution.fi>
2018-09-05 11:41:20 +0300

    Watch for file-changes and update table.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const _ = require('lodash');
+
 const bodyParser = require('body-parser');
 const parser = require('fast-xml-parser');
+const sseExpress = require('sse-express');
 const chokidar = require('chokidar');
 const iconv = require('iconv-lite');
 const express = require('express');
@@ -11,7 +13,6 @@ const fs = require('fs');
 const HOST = '0.0.0.0';
 const PORT = 8080;
 
-const class2text = require('./src/class2text.js');
 const ResultsCollector = require('./src/ResultsCollector.js');
 
 const noResponse = `<?xml version="1.0" encoding="ISO-8859-1"?>
@@ -27,107 +28,169 @@ const noResponse = `<?xml version="1.0" encoding="ISO-8859-1"?>
 	<sanoma></sanoma>
 </kehys>`;
 
-const basePath = './responses';
+const app = express();
 
-const responses = fs.readdirSync(basePath);
+function onResponse (file) {
+	const filepath = path.join(responsesPath, file);
+	const exist = fs.existsSync(filepath);
 
-if (responses) {
-	process.stdout.write(`preparing responses (${responses.length}) ... `);
+	return {
+		add (emit = false) {
+			if (exist) {
+				const response = fs.readFileSync(filepath);
 
-	// initialize responses
-	_.each(responses, file => {
-		const response = fs.readFileSync(path.join(basePath, file));
+				if (response) {
+					ResultsCollector.addOrUpdate(file, iconv.decode(response, 'latin1'));
 
-		if (file && response) {
-			const text = iconv.decode(response, 'latin1');
-			ResultsCollector.addOrUpdate(file, text);
-		}
-	});
-
-	process.stdout.write('done\n');
-
-	const app = express();
-	app.use(bodyParser.raw({
-		type: 'text/xml',
-		inflate: true,
-		limit: '100kb',
-	}));
-
-	function getReport (req, res) {
-		res.set('Content-Type', 'text/html').sendFile(path.resolve('./src/responses.html'));
-	}
-
-	function getResponse (req, res) {
-		let request, json;
-
-		if (request = req.body) {
-			request = request.toString('utf8');
-
-			res.set('Content-Type', 'text/plain');
-
-			if (json = parser.parse(request)) {
-				const hakutiedot = _.get(json, 'kehys.sanoma.ajoneuvonHakuehdot');
-
-				if (hakutiedot) {
-					const reg = hakutiedot.rekisteritunnus;
-					const vin = hakutiedot.valmistenumero;
-					let response;
-
-					res.set('Content-Type', 'application/xml');
-
-					if (response = ResultsCollector.getByReg(reg)) {
-						res.end(response);
-					} else if (response = ResultsCollector.getByVin(vin)) {
-						res.end(response);
-					} else {
-						res.end(noResponse);
+					if (emit) {
+						app.emit('response:add', {
+							value: ResultsCollector.parsed[file],
+							key: file,
+						});
 					}
 				}
-			} else {
-				process.stderr.write(`${req.ip} parse failed.\n`);
-				res.end('parse failed.');
 			}
-		} else {
-			process.stderr.write(`${req.ip} no body.\n`);
-			res.end('no body.');
+		},
+		remove (emit = false) {
+			ResultsCollector.remove(file);
+
+			if (emit) {
+				app.emit('response:rm', file);
+			}
 		}
 	}
-
-	app.get('/xml/:name', (req, res) => {
-		res.sendFile(req.params.name, {
-			dotfiles: 'deny',
-			root: basePath,
-			headers: {
-				'Content-Type': 'application/xml; charset=ISO-8859-1',
-			}
-		});
-	});
-
-	app.get('/responses.json', (req, res) => {
-		const items = _.mapValues(ResultsCollector.parsed, root => {
-			const historia = _.get(root, 'kehys.sanoma.ajoneuvontiedot.historia');
-			const laaja = _.get(root, 'kehys.sanoma.ajoneuvontiedot.laaja');
-			const virhe = _.get(root, 'kehys.yleinen.virhe');
-
-			return {
-				error: virhe,
-				type: laaja ? 'Laaja' : historia ? 'Historia' : '?',
-				data: laaja || historia,
-			}
-		});
-
-		res.send(items);
-	});
-
-	app.get('/report', getReport);
-	app.get('/', getReport);
-
-	app.post('/index.php', getResponse);
-	app.post('/', getResponse);
-
-	const server = http.createServer(app);
-
-	server.listen(PORT, HOST);
-
-	process.stdout.write(`listening to ${HOST}:${PORT}\n`);
 }
+
+const responsesPath = './responses';
+
+const responses = fs.readdirSync(responsesPath);
+const watcher = chokidar.watch(responsesPath, {
+	ignoreInitial: true,
+	persistent: true,
+});
+
+watcher.on('all', (event, filepath) => {
+	const basename = path.basename(filepath);
+	const response = onResponse(basename);
+
+	console.log(event, basename);
+
+	switch (event) {
+		case 'add':
+		case 'change':
+			response.add(true);
+			break;
+		case 'unlink':
+			response.remove(true);
+			break;
+	}
+});
+
+process.stdout.write(`preparing responses (${responses.length}) ... `);
+
+// initialize responses
+_.each(responses, file => {
+	onResponse(file).add();
+});
+
+process.stdout.write('done\n');
+
+app.use(bodyParser.raw({
+	type: 'text/xml',
+	inflate: true,
+	limit: '100kb',
+}));
+
+function getReport (req, res) {
+	res.set('Content-Type', 'text/html').sendFile(path.resolve('./src/responses.html'));
+}
+
+function getResponse (req, res) {
+	let request, json;
+
+	if (request = req.body) {
+		request = request.toString('utf8');
+
+		res.set('Content-Type', 'text/plain');
+
+		if (json = parser.parse(request)) {
+			const hakutiedot = _.get(json, 'kehys.sanoma.ajoneuvonHakuehdot');
+
+			if (hakutiedot) {
+				const reg = hakutiedot.rekisteritunnus;
+				const vin = hakutiedot.valmistenumero;
+				let response;
+
+				res.set('Content-Type', 'application/xml');
+
+				if (response = ResultsCollector.getByReg(reg)) {
+					res.end(response);
+				} else if (response = ResultsCollector.getByVin(vin)) {
+					res.end(response);
+				} else {
+					res.end(noResponse);
+				}
+			}
+		} else {
+			process.stderr.write(`${req.ip} parse failed.\n`);
+			res.end('parse failed.');
+		}
+	} else {
+		process.stderr.write(`${req.ip} no body.\n`);
+		res.end('no body.');
+	}
+}
+
+function toResponseItem (response) {
+	const historia = _.get(response, 'kehys.sanoma.ajoneuvontiedot.historia');
+	const laaja = _.get(response, 'kehys.sanoma.ajoneuvontiedot.laaja');
+	const virhe = _.get(response, 'kehys.yleinen.virhe');
+
+	return {
+		error: virhe,
+		type: laaja ? 'Laaja' : historia ? 'Historia' : '?',
+		data: laaja || historia,
+	}
+}
+
+app.get('/xml/:name', (req, res) => {
+	res.sendFile(req.params.name, {
+		dotfiles: 'deny',
+		root: responsesPath,
+		headers: {
+			'Content-Type': 'application/xml; charset=ISO-8859-1',
+		}
+	});
+});
+
+app.get('/data.json', (req, res) => {
+	res.send({
+		responses: _.mapValues(ResultsCollector.parsed, toResponseItem),
+		class2text: require('./src/class2text.js'),
+	});
+});
+
+app.get('/event/responses', sseExpress, (req, res) => {
+	app.on('response:add', data => {
+		res.sse('add-response', {
+			value: toResponseItem(data.value),
+			key: data.key,
+		});
+	});
+
+	app.on('response:rm', data => {
+		res.sse('remove-response', data);
+	});
+});
+
+app.get('/report', getReport);
+app.get('/', getReport);
+
+app.post('/index.php', getResponse);
+app.post('/', getResponse);
+
+const server = http.createServer(app);
+
+server.listen(PORT, HOST);
+
+process.stdout.write(`listening to ${HOST}:${PORT}\n`);
